8b118366d768ca2f632f8eef4e40d109fc2cac62
Akshat Prasad <akshatpsd2005@gmail.com>
2026-06-20 11:04:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,9 @@
-const CACHE_NAME = "horizon-gallery-v1.0.0";
+const CACHE_NAME =
+    "horizon-gallery-v1.0.0";
 
 const FILES_TO_CACHE = [
-    "./",
+
+     "./",
     "./index.html",
     "./style.css",
     "./script.js",
@@ -19,89 +21,144 @@ const FILES_TO_CACHE = [
     "https://akshat-881236.github.io/TrackerJS/TrackID/seo-runtime-auditor.js",
     "https://akshat-881236.github.io/TrackerJS/TrackID/anh-inspection-mode.js",
     "https://akshat-881236.github.io/TrackerJS/redirect.js"
+
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener(
+    "install",
+    event => {
 
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-    );
+        event.waitUntil(
 
-    self.skipWaiting();
-});
+            caches.open(CACHE_NAME)
+                .then(cache =>
+                    cache.addAll(
+                        FILES_TO_CACHE
+                    )
+                )
 
-self.addEventListener("activate", event => {
+        );
 
-    event.waitUntil(
+        self.skipWaiting();
 
-        caches.keys()
-            .then(keys => {
+    }
+);
 
-                return Promise.all(
+self.addEventListener(
+    "activate",
+    event => {
 
-                    keys.map(key => {
+        event.waitUntil(
 
-                        if (key !== CACHE_NAME) {
-                            return caches.delete(key);
+            caches.keys()
+                .then(keys =>
+
+                    Promise.all(
+
+                        keys.map(key => {
+
+                            if (
+                                key !==
+                                CACHE_NAME
+                            ) {
+
+                                return caches.delete(
+                                    key
+                                );
+
+                            }
+
+                        })
+
+                    )
+
+                )
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        if (
+            event.request.method !==
+            "GET"
+        ) {
+            return;
+        }
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+            .then(cached => {
+
+                const network =
+                    fetch(
+                        event.request
+                    )
+                    .then(response => {
+
+                        if (
+                            response &&
+                            response.status === 200
+                        ) {
+
+                            const clone =
+                                response.clone();
+
+                            caches.open(
+                                CACHE_NAME
+                            )
+                            .then(cache => {
+
+                                cache.put(
+                                    event.request,
+                                    clone
+                                );
+
+                            });
+
                         }
 
-                    })
+                        return response;
 
+                    })
+                    .catch(() =>
+                        cached
+                    );
+
+                return (
+                    cached ||
+                    network
                 );
 
             })
 
-    );
+        );
 
-    self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-
-    if (event.request.method !== "GET") {
-        return;
     }
+);
 
-    event.respondWith(
+self.addEventListener(
+    "message",
+    event => {
 
-        caches.match(event.request)
-            .then(cachedResponse => {
+        if (
+            event.data &&
+            event.data.type ===
+            "SKIP_WAITING"
+        ) {
 
-                const networkFetch = fetch(event.request)
+            self.skipWaiting();
 
-                    .then(networkResponse => {
+        }
 
-                        if (
-                            networkResponse &&
-                            networkResponse.status === 200
-                        ) {
-
-                            const clone =
-                                networkResponse.clone();
-
-                            caches.open(CACHE_NAME)
-                                .then(cache => {
-
-                                    cache.put(
-                                        event.request,
-                                        clone
-                                    );
-
-                                });
-
-                        }
-
-                        return networkResponse;
-
-                    })
-
-                    .catch(() => cachedResponse);
-
-                return cachedResponse || networkFetch;
-
-            })
-
-    );
-
-});
+    }
+);
